7da9757a7374ac77b5046a54ede5f206d9ff26b4
Ferit Tunçer <ferit@cryptolab.net>
2020-12-07 19:47:12 +0300

    initialize docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/home/f/repos/knowledge-base/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/f/repos/knowledge-base/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/f/repos/knowledge-base/node_modules/remark-admonitions/styles/infima.css"),
+  require("/home/f/repos/knowledge-base/node_modules/infima/dist/css/default/default.css"),
+  require("/home/f/repos/knowledge-base/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
+  require("/home/f/repos/knowledge-base/src/css/custom.css"),
+];
